20a51099fe5198052a8cd62628f9b67467178dc6
kristjankristinsson <97900321+kristjankristinsson@users.noreply.github.com>
2022-03-31 16:00:42 +0200

    rendering
    added rendering

--- students.js
+++ students.js
@@ -30,3 +30,34 @@ inputStudent.addEventListener('keyup', function () {
       document.getElementById("results").innerHTML = "";
     }
   });
+
+  function renderStudent (student) {
+    let results = document.getElementById("results");
+    let div = document.createElement("div");
+    let studentName = document.createElement("h2");
+    div.classList.add("studentContainer");
+
+    let studentCourse = getStudentCourses(student);
+    let totalCredits = studentCourse.reduce(function(a, b){return a + b}, 0);
+
+    studentName.innerText = student.firstName + " " + student.lastName + " (total credits: " + totalCredits + ")";
+    results.appendChild(studentName);
+    results.appendChild(div);
+
+    let foundCourses = getCourseById(student);
+
+
+    for (let i = 0; i < foundCourses.length; i ++){
+        let kurser = document.createElement("div");
+        kurser.classList.add("kurser");
+
+        div.appendChild(kurser);
+        kurser.innerText = foundCourses[i].title + "\n" + student.courses[i].started.semester + " " + student.courses[i].started.year + " / " 
+        + student.courses[i].passedCredits + " of " + foundCourses[i].totalCredits + " credits";
+        
+        if (foundCourses[i].totalCredits == student.courses[i].passedCredits){
+            kurser.style.backgroundColor = "green";
+        }
+    }
+    
+}
