68f60a618b9a287802717e54572d3e70bc4a215f
kristjankristinsson <97900321+kristjankristinsson@users.noreply.github.com>
2022-03-31 16:05:15 +0200

    Course and teachers
    added function for teachers and courses

--- courses.js
+++ courses.js
@@ -27,3 +27,71 @@ inputCourse.addEventListener("keyup", function(){
         document.getElementById("results").innerHTML = "";
     }
 });
+
+
+function renderaKurs (course) {
+    let results = document.getElementById("results");
+    let div = document.createElement("div");
+    let courseTitle = document.createElement("h2");
+    div.classList.add("studentContainer");
+
+    courseTitle.innerText = course.title + " (" + course.totalCredits + " credits)";
+    results.appendChild(courseTitle);
+    results.appendChild(div);
+// kurs lärare 
+    let foundStudents = getStudentsById(course);
+    let huvudLararen = getResponsibleTeacher(course);
+    let allTeachers = getAllTeachers(course);
+    let studentCourses = [];
+    
+    for (let student of foundStudents){
+        for (let studentCourse of student.courses){
+            if (studentCourse.courseId == course.courseId){
+                studentCourses.push(studentCourse);
+            }
+        }
+    }
+
+    for (let i = 0; i < foundStudents.length; i ++){
+        let studenter = document.createElement("div");
+        studenter.classList.add("studenter");
+
+        div.appendChild(studenter);
+        studenter.innerText = foundStudents[i].firstName + " " + foundStudents[i].lastName + " (" + studentCourses[i].passedCredits +" credits)" + "\n" 
+        + "Started: "+ studentCourses[i].started.semester + " " + studentCourses[i].started.year; 
+        
+        if (studentCourses[i].passedCredits == course.totalCredits){
+            studenter.style.backgroundColor = "green";
+        }
+    }
+
+    let repsonsibleDiv = document.createElement("div"); 
+    repsonsibleDiv.innerHTML = "Course responsible: ";
+    repsonsibleDiv.classList.add("ansvarig");
+    div.appendChild(repsonsibleDiv);
+
+
+    for (let i = 0; i < huvudLararen.length; i++) {
+        let respP = document.createElement("p");
+        respP.classList.add("respP");
+        repsonsibleDiv.appendChild(respP);
+        respP.innerText = huvudLararen[i].firstName + " " + huvudLararen[i].lastName + " (" + huvudLararen[i].post + ")";
+    }
+
+    let larare = document.createElement("div");
+    larare.classList.add("larare");
+    div.appendChild(larare);
+    larare.innerText = "Teachers: ";
+
+    for(let i = 0; i < allTeachers.length; i++){
+        let teacherP = document.createElement("p");
+        larare.appendChild(teacherP);
+        teacherP.innerText =  allTeachers[i].firstName + " " + allTeachers[i].lastName + " (" + allTeachers[i].post + ")";
+    }
+}
+
+function skapaHTML (courses) {
+    for (let course of courses){
+        renderaKurs(course);
+    }
+}
